6af284ec24da4b3b8e48c70392a53e332d1517fc
asif1-3ahmed <mdasifahmed07042006@gmail.com>
2025-11-08 19:24:05 +0530

    video

--- frontend/src/components/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-
-export default function PrivateRoute(){
-  const token = localStorage.getItem("token");
-  const user = JSON.parse(localStorage.getItem("user") || "null");
-  if(!token || !user) return <Navigate to="/" replace />;
-  return <Outlet />;
-}
