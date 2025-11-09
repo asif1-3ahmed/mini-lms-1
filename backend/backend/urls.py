75fc94a2f9621bfccb1754cd0e203a7593f14c84
asif1-3ahmed <mdasifahmed07042006@gmail.com>
2025-11-09 19:48:20 +0530

    urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/accounts/", include("accounts.urls")),
-    path("api/weeks/", include("course.week_urls")),
+    path("api/weeks/", include("courses.week_urls")),
     path("api/courses/", include("courses.course_urls")),
 
 ]
